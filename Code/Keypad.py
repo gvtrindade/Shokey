5df12decdcda239c10e7f40e2a58ba0e4f2b8d9d
gvtrindade <gabrielsvtrindade@gmail.com>
2021-11-30 13:44:42 -0300

    Disconnection handler implemented

--- Code/Keypad.py
+++ Code/Keypad.py
@@ -1,46 +1,61 @@
 import sys
-import subprocess 
-import pynput 
-import serial.tools.list_ports # Imported from pyserial
+import subprocess
+import os
+import time
+import pynput
+import serial.tools.list_ports  # Imported from pyserial
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
-#Start keyboard and mouse controllers, key and button classes
+# Start keyboard and mouse controllers, key and button classes
 KEYBOARD = pynput.keyboard.Controller()
 MOUSE = pynput.mouse.Controller()
 KBKEY = pynput.keyboard.Key
 MSBUTTON = pynput.mouse.Button
 
-#Define to which port the keypad is connected 
-VENDORID = 9026 #converted from hexadecimal: 0x2342
-PRODUCTID = 32825 #converted from hexadecimal:0x8039
-
-def ports_in_use():
-    return serial.tools.list_ports.comports()
-
-
-def test_connection():
-    portFound = any((p.vid == VENDORID and p.pid == PRODUCTID) for p in ports_in_use())
-    return portFound
-
-
-def find_used_port():
-    for p in ports_in_use():
-        if (p.vid == VENDORID and p.pid == PRODUCTID):
-            return p.name            
-
-
-#Get shortcut list from file
+# Get shortcut list from file
 shortcuts = {}
-def define_values():
-    file = open('shortcuts.txt','rt')
+currentDirectory = os.getcwd() + '\Code\shortcuts.txt'
+def set_shortcuts():
+    file = open(currentDirectory, 'rt')
     for line in file:
         if line.strip():
             key, val = line.strip().split(';')
             shortcuts[key.strip()] = val.strip()
 
 
-#Functions to handle the type of command to be executed
+# Define to which port the keypad is connected
+VENDORID = 9026  # converted from hexadecimal: 0x2342
+PRODUCTID = 32825  # converted from hexadecimal:0x8039
+
+def ports_in_use():
+    return serial.tools.list_ports.comports()
+
+
+def test_connection():
+    portFound = any((p.vid == VENDORID and p.pid == PRODUCTID)
+                    for p in ports_in_use())
+    return portFound
+
+
+def find_used_port():
+    for p in ports_in_use():
+        if (p.vid == VENDORID and p.pid == PRODUCTID):
+            return p.name
+
+    return False
+
+
+# Establish serial connection between the arduino and the app
+def establish_connection(port):
+    return serial.Serial(port, 9600)
+
+
+def send_serial_msg(serialcomm, msg):
+    serialcomm.write(msg.encode())
+
+
+# Handle the type of command to be executed
 def key_command(pressedKey):
     if (len(pressedKey) > 1):
         KEYBOARD.tap(getattr(KBKEY, pressedKey))
@@ -53,8 +68,8 @@ def combo_command(comboToPress):
     lastKey = comboToPress[len(comboToPress) - 1]
     if (len(comboToPress) > 2):
         secondKey = KEYBOARD.pressed(getattr(KBKEY, comboToPress[1]))
-        with firstKey:                
-            with secondKey:                
+        with firstKey:
+            with secondKey:
                 key_command(lastKey)
     else:
         with firstKey:
@@ -62,7 +77,7 @@ def combo_command(comboToPress):
 
 
 def txt_command(txtSent):
-    KEYBOARD.type(txtSent)            
+    KEYBOARD.type(txtSent)
 
 
 def link_command(linkSent):
@@ -71,58 +86,21 @@ def link_command(linkSent):
 
 def scroll_command(valuesSent):
     print(valuesSent)
-    MOUSE.scroll(int(valuesSent[0]),int(valuesSent[1]))
+    MOUSE.scroll(int(valuesSent[0]), int(valuesSent[1]))
 
 
-#Handle the serial connection between the arduino and the program 
-def establish_connection(port):
-    return serial.Serial(port, 9600)
-
-
-def send_serial_msg(msg):
-    serialcomm.write(msg.encode())
-
-
-serialcomm = establish_connection(find_used_port())
-send_serial_msg('I')
-
-class SystemTrayIcon(QSystemTrayIcon):
-    def __init__(self, icon, parent=None):
-        QSystemTrayIcon.__init__(self, icon, parent)
-        self.setToolTip('Shokey')
-        menu = QMenu(parent)
-        open_app = menu.addAction("Exit")
-        open_app.triggered.connect(self.exit_function)
-
-        menu.addSeparator()
-        self.setContextMenu(menu)
-
-    def exit_function(self):
-        sys.exit()
-
-def main():
-    app = QApplication(sys.argv)
-    w = QWidget()
-    tray_icon = SystemTrayIcon(QIcon("icon.png"), w)
-    tray_icon.show()
-    sys.exit(app.exec_())
-
-# if __name__ == '__main__':
-#     main()
-
-
-while True:
-
-    define_values()
+def communication_handler(serialcomm):
+    set_shortcuts()
     var = serialcomm.readline().decode('ascii').strip()
     shortcut = shortcuts.get(var, -1)
 
     if (var[0] == '0'):
-        send_serial_msg('O')
+        print('Closing app...')
+        send_serial_msg(serialcomm, 'O')
         serialcomm.close()
         sys.exit()
 
-    if shortcut != -1:
+    elif (shortcut != -1):
 
         if (shortcut[:3] == 'key'):
             key_command(shortcut[4:])
@@ -139,6 +117,35 @@ while True:
 
         if (shortcut[:6] == 'scroll'):
             scrollDirections = shortcut[7:].split(',')
-            scroll_command(scrollDirections)               
-
+            scroll_command(scrollDirections)
     del var
+
+
+#Searching for the arduino and updating the communication 
+sleep_amount = 0.8
+
+while (not find_used_port()):
+    find_used_port()
+    time.sleep(sleep_amount)
+
+def update_communication():
+
+    serialcomm = establish_connection(find_used_port())
+    send_serial_msg(serialcomm, 'I')
+
+    while True:
+        try:
+            communication_handler(serialcomm)
+
+        except SystemExit:
+            break
+
+        except:
+            print(sys.exc_info()[0])
+            find_used_port()
+            if(find_used_port()):
+                update_communication()
+            time.sleep(sleep_amount)
+
+
+update_communication()
--- Code/test.py
+++ Code/test.py
@@ -1,52 +1,7 @@
-import os
-import sys
-from PyQt5.QtGui import * 
-from PyQt5.QtWidgets import * 
+shortcuts = {}
+def define_values():
+    file = open("C:\Users\GabrielTrindade\Documents\Stuff\Python\Shokey\Code\Best.txt","rt")
+    for line in file:
+        print(line)
 
-
-class SystemTrayIcon(QSystemTrayIcon):
-    """
-    CREATE A SYSTEM TRAY ICON CLASS AND ADD MENU
-    """
-    def __init__(self, icon, parent=None):
-        QSystemTrayIcon.__init__(self, icon, parent)
-        self.setToolTip('Shokey')
-        menu = QMenu(parent)
-        open_app = menu.addAction("Open Notepad")
-        open_app.triggered.connect(self.open_notepad)
-
-        open_cal = menu.addAction("Open Calculator")
-        open_cal.triggered.connect(self.open_calc)
-
-        exit_ = menu.addAction("Exit")
-        exit_.triggered.connect(lambda: sys.exit())
-
-        menu.addSeparator()
-        self.setContextMenu(menu)
-        self.activated.connect(self.onTrayIconActivated)
-
-    def onTrayIconActivated(self, reason):
-        if reason == self.DoubleClick:
-            self.open_notepad()
-
-
-    def open_notepad(self):
-        sys.exit()
-
-    def open_calc(self):
-        os.system('calc')
-
-
-def main():
-    app = QApplication(sys.argv)
-    w = QWidget()
-    tray_icon = SystemTrayIcon(QIcon("icon.png"), w)
-    tray_icon.show()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
-
-while True:
-    me = "dumb"
+define_values()
