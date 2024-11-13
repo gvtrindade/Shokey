5e5fc1ed99c561f9d244f6a86a373ad2ea8c5b12
Gabriel Trindade <gabrielsvtrindade@gmail.com>
2024-11-12 21:43:29 -0300

    chore: remove unused files

--- Software/src/communication.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-def establish_connection(port):
-    return serial.Serial(f"/dev/{port}", 115200, timeout=1000)
-
-
-def send_serial_msg(serialcomm, msg):
-    serialcomm.write(msg.encode())
